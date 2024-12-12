a5d4ca6379cea10f1eac6c0d3395eef0c66e4ff1
Robert johansson <bloodmier@gabenpirates.com>
2024-12-12 20:26:33 +0100

    Se om detta fungerar

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
+const { ObjectId } = require('mongoose').Types;
 
 // GET all products
 router.get('/', async (req, res) => {
