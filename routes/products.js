4ecc930f0e6404b287b4cec1328f8dd677977dd2
Robert johansson <bloodmier@gabenpirates.com>
2024-12-12 15:51:19 +0100

    first commit including get product by id

---
+++ routes/products.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+// GET all products
+router.get('/', async (req, res) => {
+    try {
+        res.json(await Product.find());
+    } catch(error) {
+        res.json({message: error});
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        res.json(await Product.findById(req.params.id));
+    } catch(error) {
+        res.json({message: error});
+    }
+});
+
+module.exports = router;
