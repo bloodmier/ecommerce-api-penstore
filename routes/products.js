cb3d3c93ec33f31a74f9a59370bfefadfba7eac1
Robert johansson <bloodmier@gabenpirates.com>
2024-12-13 10:50:50 +0100

    Regex

--- routes/products.js
+++ routes/products.js
@@ -9,29 +9,25 @@ router.get('/', async (req, res) => {
         const products = await Product.find();
         res.json(products);
     } catch (error) {
-        console.error('Fel vid hämtning av produkter:', error);
         res.status(500).json({ message: error.message });
     }
 });
 
 // GET product by ID
 router.get('/:id', async (req, res) => {
-    console.log('Förfrågan mottagen med ID:', req.params.id);
-    if (!ObjectId.isValid(req.params.id)) {
-        console.log('Felaktigt ID-format');
+    const id = req.params.id.replace(/[<>]/g, ''); 
+
+    if (!ObjectId.isValid(id)) {
         return res.status(400).json({ message: 'Felaktigt ID-format' });
     }
 
     try {
-        const product = await Product.findById(req.params.id);
-        console.log('Produkt funnen:', product);
+        const product = await Product.findById(id);
         if (!product) {
-            console.log('Produkt inte hittad');
             return res.status(404).json({ message: 'Produkt inte hittad' });
         }
         res.json(product);
     } catch (error) {
-        console.error('Fel vid hämtning av produkt:', error);
         res.status(500).json({ message: error.message });
     }
 });
